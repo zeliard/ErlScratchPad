13c6889754857b4e3cfeecafbeecac78709acb93
Simon Koo <cyberjam@gmail.com>
2014-09-21 18:17:22 +0900

    *impl: simple client

--- src/echotest.erl
+++ src/echotest.erl
@@ -1,8 +1,11 @@
 -module(echotest).
 -export([start_server/0]).
+-export([start_client/0]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, true}, {reuseaddr, true}]).
 -define(PORT, 9001).
+-define(HOST, "127.0.0.1").
+-define(DATASIZE, 4000).
 
 start_server() ->
 	{ok, ListenSocket} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
@@ -27,3 +30,13 @@ loop(Socket) ->
 	    {tcp_error, Socket, Reason} ->
 	        io:format("Error on socket ~p reason: ~p~n", [Socket, Reason])
     end.
+
+start_client() ->
+    {ok, Sock} = gen_tcp:connect(?HOST, ?PORT, ?TCP_OPTIONS),
+    Pid = spawn(fun() -> 
+        io:format("Connection established ~n", []),
+        loop(Sock) 
+    end),
+    gen_tcp:controlling_process(Sock, Pid),
+    Message = crypto:strong_rand_bytes(?DATASIZE),
+    gen_tcp:send(Sock, Message).
